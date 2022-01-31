3597fae41a85dbbcfce3666348c0336b9184a070
Tiffany Zhu <75339798+Tiffany-Zhu5303@users.noreply.github.com>
2022-01-31 15:56:43 +0000

    work in progress

---
+++ lab1/month.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+  //ask user to input the year and month (1-12)
+  int year, month;
+
+  std::cout << "Enter year: ";
+  std::cin >> year;
+
+  std::cout << "Enter month (1-12): ";
+  std::cin >> month;
+
+  //print number of days in that month
+  
+  return 0;
+}
