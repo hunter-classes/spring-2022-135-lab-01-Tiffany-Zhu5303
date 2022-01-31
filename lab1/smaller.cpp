74f1cc6c03efacf117ccac8305ab7794d709e0b9
Tiffany Zhu <75339798+Tiffany-Zhu5303@users.noreply.github.com>
2022-01-31 14:22:19 +0000

    started lab1

---
+++ lab1/smaller.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+  //std::cout << "Hello world!\n";
+
+  //ask user to input two numbers 
+  int a, b;
+
+  std::cout << "Enter a number:\n";
+  std::cin >> a;
+
+  std::cout << "Enter another number:\n";
+  std::cin >> b;
+
+  //return the smallest of the two
+  /*if (a<b){
+    std::cout << "The smallest number is " << a;
+  } else (b<a){
+    std::cout << "The smallest number is " << b;
+  }*/
+  
+  return 0;
+}
