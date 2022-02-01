cc7fd2c504a6f5b5d36b5600397b00ea6a3ad866
Tiffany Zhu <75339798+Tiffany-Zhu5303@users.noreply.github.com>
2022-02-01 03:42:45 +0000

    task d

--- month.cpp
+++ month.cpp
@@ -12,5 +12,15 @@ int main(){
 
   //print number of days in that month
   
+  if (year % 4 != 0){
+    std::cout << "Common year";
+  }else if (year % 100 != 0){
+    std::cout << "Leap year";
+  }else if (year % 400 != 0){
+    std::cout << "Common year";
+  }else {
+    std::cout << "Leap year";
+  }
+
   return 0;
 }
