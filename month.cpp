9741e1342c983e9dab6bc02b70cbe718acbad4d1
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2022-02-03 22:06:21 -0500

    removed int month

--- month.cpp
+++ month.cpp
@@ -2,13 +2,10 @@
 
 int main(){
   //ask user to input the year and month (1-12)
-  int year, month;
+  int year;
 
   std::cout << "Enter year: ";
   std::cin >> year;
-
-  std::cout << "Enter month (1-12): ";
-  std::cin >> month;
 
   //print number of days in that month
   
